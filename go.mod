b51cb5b6fe27d023c7f8d6727b52aa0888e93518
Ke Wang <197725416@qq.com>
2020-09-10 15:36:21 +0800

    Added a new func called Add

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bobwang189/mypackage
 
 go 1.14
+
+require github.com/shopspring/decimal v1.2.0
--- mypackage.go
+++ mypackage.go
@@ -2,8 +2,15 @@ package mypackage
 
 import (
 	"fmt"
+
+	"github.com/shopspring/decimal"
 )
 
 func New() {
 	fmt.Println("mypackage.New()")
+}
+
+func Add(num1, num2 float64) decimal.Decimal {
+	d1 := decimal.NewFromFloat(num1).Add(decimal.NewFromFloat(num2))
+	return d1
 }
